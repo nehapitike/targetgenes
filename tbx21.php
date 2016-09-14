4c3f8877a9c4b56d0a3e65f8760f5d00f1333c5c
neha Gadigi <nehapitike@gmail.com>
2016-09-13 22:54:37 -0400

    version1

--- tbx21.php
+++ tbx21.php
@@ -4,7 +4,7 @@
         switch ($tf) {
             case "TBX21":
                 # code...
-                $filename = "TBX21targetgenelist.csv";
+                $filename = "data/TBX21targetgenelist.csv";
                 $f = fopen($filename, "r");
                 $isHeaderLine = true;
                 $genelist = [];
@@ -12,20 +12,113 @@
                 $i = 0;
                 while(($line = fgets($f)) != false)
                 {
-                    
+                    if($isHeaderLine){
+                        $isHeaderLine = false;
+                    }
+                    else
+                    {
                     $lineArray = explode(",", $line);
                     //echo json_encode($lineArray);
                     $list["gene"] = trim($lineArray[0]);
                     $list["signalvalue"] =trim($lineArray[1]);
                     $genelist[$i] = $list;
                     $i = $i +1;
+                    }
+                    
                 }
                 fclose($f);
 
                 echo json_encode($genelist);
 
                 break;
-            
+
+            case "JUNB":
+                # code...
+                $filename = "data/JUNBtargetgenelist.csv";
+                $f = fopen($filename, "r");
+                $isHeaderLine = true;
+                $genelist = [];
+                $list = [];
+                $i = 0;
+                while(($line = fgets($f)) != false)
+                {
+                    if($isHeaderLine){
+                        $isHeaderLine = false;
+                    }
+                    else
+                    {
+                    $lineArray = explode(",", $line);
+                    //echo json_encode($lineArray);
+                    $list["gene"] = trim($lineArray[0]);
+                    $list["signalvalue"] =trim($lineArray[1]);
+                    $genelist[$i] = $list;
+                    $i = $i +1;
+                    }
+                    
+                }
+                fclose($f);
+
+                echo json_encode($genelist);
+
+                break;
+            case "IRF3":
+                # code...
+                $filename = "data/IRF3targetgenelist.csv";
+                $f = fopen($filename, "r");
+                $isHeaderLine = true;
+                $genelist = [];
+                $list = [];
+                $i = 0;
+                while(($line = fgets($f)) != false)
+                {
+                    if($isHeaderLine){
+                        $isHeaderLine = false;
+                    }
+                    else
+                    {
+                    $lineArray = explode(",", $line);
+                    //echo json_encode($lineArray);
+                    $list["gene"] = trim($lineArray[0]);
+                    $list["signalvalue"] =trim($lineArray[1]);
+                    $genelist[$i] = $list;
+                    $i = $i +1;
+                    }
+                    
+                }
+                fclose($f);
+
+                echo json_encode($genelist);
+
+                break;
+            case "SMAD5":
+                # code...
+                $filename = "data/SMAD5targetgenelist.csv";
+                $f = fopen($filename, "r");
+                $isHeaderLine = true;
+                $genelist = [];
+                $list = [];
+                $i = 0;
+                while(($line = fgets($f)) != false)
+                {
+                    if($isHeaderLine){
+                        $isHeaderLine = false;
+                    }
+                    else
+                    {
+                    $lineArray = explode(",", $line);
+                    //echo json_encode($lineArray);
+                    $list["gene"] = trim($lineArray[0]);
+                    $list["signalvalue"] =trim($lineArray[1]);
+                    $genelist[$i] = $list;
+                    $i = $i +1;
+                    }
+                    
+                }
+                fclose($f);
+
+                echo json_encode($genelist);
+
+                break;
             default:
                 # code...
                 echo "No Data Yet";
